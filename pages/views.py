921baafa5b7f6639e616f03b8f89db0e58d6390c
aali195 <amd5910@gmail.com>
2019-03-08 04:41:19 +0000

    Implement basic template and base for 'index' and 'about' pages

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
