9e5f28e42a58918dda9669a90572ae0049d1e3aa
aali195 <amd5910@gmail.com>
2019-03-09 03:26:33 +0000

    Add collections model and Pillow as dependancy for images

--- collecs/models.py
+++ collecs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
+from django.conf import settings
 
-# Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length=50)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    image = models.ImageField(upload_to='collections/%Y/%m', default='no-image.png')
+    description = models.TextField(blank=True)
+    size = models.IntegerField(default=0)
+    is_visible = models.BooleanField(default=False)
+    upload_date = models.DateTimeField(default=datetime.now)
+    last_update = models.DateTimeField(default=datetime.now)
+    rating = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
