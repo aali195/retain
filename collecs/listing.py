f0f214958a8469864fd571c0dbf5ddbca51f088e
aali195 <amd5910@gmail.com>
2019-03-12 23:51:14 +0000

    Implement displaying correct collection info

---
+++ collecs/listing.py
@@ -0,0 +1,11 @@
+from django.shortcuts import get_object_or_404
+
+from .models import Collection
+
+def get_collection(request, collection_id):
+    collection = get_object_or_404(Collection, pk=collection_id)
+
+    context = {
+        'collection': collection
+    }
+    return context
--- collecs/views.py
+++ collecs/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 
 from .listings import list_collections
 from .search import search_collections
+from .listing import get_collection
 
 def index(request):
     context = list_collections(request)
     return render(request, 'collecs/collections.html', context)
-
+    
 def collection(request, collection_id):
-    return render(request, 'collecs/collection.html')
+    context = get_collection(request, collection_id)
+    return render(request, 'collecs/collection.html', context)
 
 def search(request):
     context = search_collections(request)
