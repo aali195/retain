04924ea11a89e1671ba99e163ad667cc50d12541
aali195 <amd5910@gmail.com>
2019-03-13 16:23:13 +0000

    Implement a user alert function

--- retain/settings.py
+++ retain/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from secret import SECRET_KEY, DATABASES
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,8 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/' 
+
+# Messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
