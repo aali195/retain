74140a82eb813e2e4dc07945229908eb6d3e87dc
aali195 <amd5910@gmail.com>
2019-03-13 07:06:11 +0000

    Implement basic 'users' app templates with linking

--- retain/urls.py
+++ retain/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('users/', include('users.urls')),
     path('collections', include('collecs.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def register(request):
+    return render(request, 'users/register.html')
+
+def login(request):
+    return render(request, 'users/login.html')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
