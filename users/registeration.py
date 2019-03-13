a82ee6541b47629d640edb41489012718a7a539c
aali195 <amd5910@gmail.com>
2019-03-13 22:20:08 +0000

    Implement user registeration

---
+++ users/registeration.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def register_user(request):
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    result = check_input(username, email, password, password2)
+
+    if result != 1:
+        return result
+    else:
+        return save_user(username, email, password, password2)
+
+def check_input(username, email, password, password2):
+    if password != password2:
+        return 'Passwords do not much'
+    else:
+        if User.objects.filter(username=username).exists():
+            return 'Username is taken'
+        else:
+            if User.objects.filter(email=email).exists():
+                return 'Email is taken'
+            else:
+                # Success
+                return 1
+
+def save_user(username, email, password, password2):
+    user = User.objects.create_user(username=username, password=password, email=email)
+    user.save()
+    return 1
--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .registeration import register_user
 
 def register(request):
     if request.method == 'POST':
-        # Register User
-        return 
+        register_status = register_user(request)
+        if register_status != 1:
+            messages.error(request, register_status)
+            return redirect('register')
+        else:
+            messages.success(request, 'You are now registered and can login')
+            return redirect('login')
     else:
         return render(request, 'users/register.html')
 
