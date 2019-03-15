c85c950caa35685d081734729f84252f2ae211d8
aali195 <amd5910@gmail.com>
2019-03-15 04:59:34 +0000

    Refactor to not repeat fields for user change form

--- users/forms.py
+++ users/forms.py
@@ -3,13 +3,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'password1', 'password2')
+
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'active_collection')
+        fields = UserCreationForm.Meta.fields + ('active_collection', )
