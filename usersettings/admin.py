fdaaf5482d93ebb6e62f1412cc9420a50347e571
aali195 <amd5910@gmail.com>
2019-03-24 07:38:22 +0000

    Remove redundant search values for 'usersettings' admin page

--- usersettings/admin.py
+++ usersettings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserSettings
+
+class UserSettingsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'active_collection', 'review_num',)
+    list_display_links = ('id',)
+    list_per_page = 25
+
+admin.site.register(UserSettings, UserSettingsAdmin)
