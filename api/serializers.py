bd1bb13abed33fdb93698846ecffbc9eb6463719
aali195 <amd5910@gmail.com>
2019-03-24 06:30:24 +0000

    Implement 'statements' API endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
 from collecs.models import Collection
+from statements.models import Statement
 
+
+class StatementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Statement
+        fields = (
+            'id',
+            'collection',
+            'image',
+            'statement', 
+            'question', 
+            'answer',
+        )
+    
 class CollectionSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Collection
         fields = (
--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound
 
-from .serializers import CollectionSerializer
+from .serializers import CollectionSerializer, StatementSerializer
 from collecs.models import Collection
+from statements.models import Statement
 
 class ListCollectionsView(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
@@ -15,3 +17,17 @@ class ListCollectionsView(viewsets.ModelViewSet):
         collection = self.get_queryset()
         serializer = self.get_serializer_class()(collection)
         return Response(serializer.data)
+        
+
+class GetStatementsView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = StatementSerializer
+    http_method_names = ['get']
+
+    def get_queryset(self):
+        collection_id = self.kwargs['collection_id']
+        statement = Statement.objects.filter(collection_id=collection_id)
+        if statement:
+            return statement
+        else:
+            raise NotFound(detail="Not found.", code=404)
