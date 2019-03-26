e444557e2b1ebd98b2f6a4fde054547f95a87d6c
aali195 <amd5910@gmail.com>
2019-03-26 12:22:06 +0000

    Revert 'usersettings' model to use 'active_collection' attribute

--- usersettings/admin.py
+++ usersettings/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import UserSettings
 
 class UserSettingsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'active_sub', 'review_num',)
+    list_display = ('id', 'active_collection', 'review_num',)
     list_display_links = ('id',)
     list_per_page = 25
 
--- usersettings/models.py
+++ usersettings/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-from subscriptions.models import Subscription
+from collecs.models import Collection
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserSettings(models.Model):
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
-    active_sub = models.ForeignKey(Subscription, on_delete=models.DO_NOTHING, null=True)
+    active_collection = models.ForeignKey(Collection, on_delete=models.DO_NOTHING, null=True)
     review_num = models.IntegerField(default=5)
