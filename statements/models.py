c5eb1fbd895cc563fe55029c3681cdae5478f715
aali195 <amd5910@gmail.com>
2019-03-31 14:39:13 +0100

    Change 'image' attribute for statements to allow blank

--- statements/models.py
+++ statements/models.py
@@ -5,7 +5,7 @@ from collecs.models import Collection
 
 class Statement(models.Model):
     collection = models.ForeignKey(Collection, on_delete=models.DO_NOTHING)
-    image = models.ImageField(upload_to='statements/%Y/%m', default='no-image.png')
+    image = models.ImageField(upload_to='statements/%Y/%m', blank=True)
     statement = models.CharField(max_length=200)
     question = models.CharField(max_length=100)
     answer = models.CharField(max_length=25)
