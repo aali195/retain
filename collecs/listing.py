128734c569e734932b5d052acc236888c29b13d6
aali195 <amd5910@gmail.com>
2019-04-02 16:49:48 +0100

    Implement statement preview on each collection template

--- collecs/listing.py
+++ collecs/listing.py
@@ -1,12 +1,16 @@
 from django.shortcuts import get_object_or_404
 
 from .models import Collection
+from statements.models import Statement
+
 
 def get_collection(request, collection_id):
     collection = get_object_or_404(Collection, pk=collection_id)
+    statements = Statement.objects.filter(collection=collection)[:10]
 
     context = {
         'collection': collection,
+        'statements': statements,
         'values': request.GET
     }
     return context
