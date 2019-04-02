18e07aa5bf4d844ccdf403a8c51c41d416958269
aali195 <amd5910@gmail.com>
2019-04-02 16:15:37 +0100

    Add more validation on learn and review sessions

--- progress/views.py
+++ progress/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+
 
 from usersettings.models import UserSettings
 from progress.models import Progress
@@ -6,21 +8,37 @@ from subscriptions.models import Subscription
 
 
 def learn(request):
-    user_settings = get_object_or_404(UserSettings, user=request.user)
-    subscription = get_object_or_404(Subscription, user=request.user, collection=user_settings.active_collection)
+    if not request.user.is_authenticated:
+        messages.error(request, 'You need to be logged in to start a learning session')
+        return redirect('index')
+    else:
 
-    context = {
-        'user_settings': user_settings,
-        'subscription': subscription,
-    }
-    return render(request, 'progress/learn.html', context)
+        user_settings = get_object_or_404(UserSettings, user=request.user)
+        subscription = get_object_or_404(Subscription, user=request.user, collection=user_settings.active_collection)
+        if subscription.completed_count == subscription.collection.size:
+            messages.error(request, 'You have completed learning the currently active collection')
+            return redirect('dashboard')
+        else:
+            context = {
+                'user_settings': user_settings,
+                'subscription': subscription,
+            }
+            return render(request, 'progress/learn.html', context)
 
 def review(request):
-    user_settings = get_object_or_404(UserSettings, user=request.user)
-    subscription = get_object_or_404(Subscription, user=request.user, collection=user_settings.active_collection)
+    if not request.user.is_authenticated:
+        messages.error(request, 'You need to be logged in to start a reviewing session')
+        return redirect('index')
+    else:
+        user_settings = get_object_or_404(UserSettings, user=request.user)
+        subscription = get_object_or_404(Subscription, user=request.user, collection=user_settings.active_collection)
 
-    context = {
-        'user_settings': user_settings,
-        'subscription': subscription,
-    }
-    return render(request, 'progress/review.html', context)
+        if subscription.completed_count < 4:
+            messages.error(request, 'You need to learn at least 4 statements before a learning session')
+            return redirect('dashboard')
+        else:
+            context = {
+                'user_settings': user_settings,
+                'subscription': subscription,
+            }
+            return render(request, 'progress/review.html', context)
