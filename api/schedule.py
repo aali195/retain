fe2432ec5daac0d73f9a1b57de0a2ce9b08f1918
aali195 <amd5910@gmail.com>
2019-04-03 06:50:33 +0100

    Implement new progresssion scheduling algorithm

--- api/schedule.py
+++ api/schedule.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, PermissionDenied
-from django.db.models import F
+from rest_framework.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
 from datetime import datetime
+from random import randint
 
 from statements.models import Statement
 from usersettings.models import UserSettings
@@ -30,29 +30,46 @@ def save_learn_result(user, statement_id, note):
 def get_next_review(user):
     user_settings = get_object_or_404(UserSettings, user=user)
     active_statements = Statement.objects.filter(collection=user_settings.active_collection)
-    if active_statements.count() < 10:
+    if user_settings.active_collection is None:
+        raise PermissionDenied(detail="No collection active.", code=403)
+    elif active_statements.count() < 10:
         raise PermissionDenied(detail="Collection has less than 10 statements.", code=403)
     else:
         user_collection_progress = Progress.objects.filter(user=user, statement__collection=user_settings.active_collection)
         if user_collection_progress.count() < 4:
             raise PermissionDenied(detail="Need to learn 4 statements before reviewing", code=403)
         else:
-            active_progress = user_collection_progress.annotate(review_incorrect=F('review_total')-F('review_correct')).order_by('review_incorrect')[0]
+            
+            active_progress = user_collection_progress.order_by('-priority')[0]
+            
             return active_progress
 
 
 def save_review_result(user, current_statement, is_correct):
     
-    progress = get_object_or_404(Progress, user=user, statement=current_statement)
-    progress.update_date = datetime.now()
-    progress.review_total += 1
-    if is_correct:
-        progress.review_correct += 1
-    progress.save()
-
     subscription = get_object_or_404(Subscription, user=user, collection=current_statement.collection)
     subscription.last_reviewed = datetime.now()
     subscription.save()
+
+    progress = get_object_or_404(Progress, user=user, statement=current_statement)
+    progress.update_date = datetime.now()
+    progress.review_total += 1
+    
+    if is_correct:
+        progress.review_correct += 1
+        if ((progress.streak +2) <= 6):
+            progress.streak += 2
+    else:
+        if (progress.streak >= -6):
+            progress.streak -= 1
+
+    variation = randint(-1, 0)
+    if (progress.priority + variation) in range(-50, 50):
+        progress.priority += variation
+    if (progress.priority - progress.streak) in range(-50, 50):
+        progress.priority -= progress.streak
+    
+    progress.save()
 
     return
 
--- progress/models.py
+++ progress/models.py
@@ -7,9 +7,9 @@ from statements.models import Statement
 class Progress(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
     statement = models.ForeignKey(Statement, on_delete=models.DO_NOTHING)
-    review_total = models.IntegerField(default=0)
-    review_correct = models.IntegerField(default=0)
-    streak = models.IntegerField(default=0)
-    priority = models.IntegerField(default=50)
     note = models.CharField(max_length=250, default='N/A')
+    priority = models.IntegerField(default=0)
+    streak = models.IntegerField(default=0)
+    review_correct = models.IntegerField(default=0)
+    review_total = models.IntegerField(default=0)
     update_date = models.DateTimeField(default=datetime.now)
